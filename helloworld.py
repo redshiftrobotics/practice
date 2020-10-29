8bad91381061d49ebd33474b3573e1e475b1104d
Julian Allchin <julian@allchin.com>
2020-10-29 16:11:11 -0700

    Libraries

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
-print("hello world")
+import wpilib
+from wpilib.drive import DifferentialDrive
+from networktables import NetworkTables
+from ctre import WPI_TalonSRX, ControlMode, NeutralMode, FeedbackDevice
+
+talon = WPI_TalonSRX(0)
+
+print(talon)
