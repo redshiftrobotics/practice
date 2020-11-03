9cd0431935f2542991b53db3f2f2bc139fc2963e
Julian Allchin <julian@allchin.com>
2020-11-02 16:11:55 -0800

    Fixed my stupidity

--- challenge.py
+++ challenge.py
@@ -1,24 +1,32 @@
 # Duplicate this file, then try to complete as much as you can. 
-
 def print_name():
     # Your code here
+    return
 
 def multiply_numbers():
     # Your code here
+    return
 
 def area_square(side_length):
     # Your code here
+
+    area = 0
     return area
 
 def numbers():
     # Your code here
+    return
 
-def passing_grade():
+def passing_grade(score):
     # Your code here
+
+    grade = 0
     return grade
 
 def mean_list():
     # Your code here
+
+    mean = 0
     return mean
 
 
@@ -26,14 +34,17 @@ def mean_list():
 # Put the car class here
 class Car:
     def __init__():
+        return
 
 def create_car():
     # Create a car here. Print color
+    return
 
 
 
 def fibonacci(n=100):
     # Challenge one. Good luck
+    return
 
 
 # To run your code, just call the function here for example
