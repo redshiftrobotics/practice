9d993fafe372fc4bea2fd970dc326b0d38a06c85
AlexanderKramer <alexanderkramer@seattleacademy.org>
2020-11-05 16:41:22 -0800

    Create alex.py

---
+++ alex.py
@@ -0,0 +1,55 @@
+# Duplicate this file, then try to complete as much as you can. 
+def print_name():
+    print("alexander kramer")
+    return
+
+def multiply_numbers():
+    f= 9983663 *8435
+    print(f)
+    return
+
+def area_square('43'):
+    y= 43*43
+    print(y)
+    return
+
+
+def numbers():
+    z=0 
+    while z <100:
+        z=z+1
+        print(z)
+    return
+
+def passing_grade(percent):
+    # Your code here
+    return
+
+def mean_list():
+    # Your code here
+    return
+
+
+
+# Put the car class here
+class Car:
+    def __init__(self):
+        return
+
+def create_car():
+    # Create a car here. Print color
+    return
+
+
+
+def fibonacci(n=100):
+    # Challenge one. Good luck
+    return
+
+
+# To run your code, just call the function here for example
+print_name()
+
+multiply_numbers()
+
+area_square(side_length)
