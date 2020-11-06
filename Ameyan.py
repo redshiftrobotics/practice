f8c7bdb58f93b659f01faae04fe82c8dec2e7a9d
ameyandash <59712061+ameyandash@users.noreply.github.com>
2020-11-05 16:04:16 -0800

    Create Ameyan.py

---
+++ Ameyan.py
@@ -0,0 +1,45 @@
+# Duplicate this file, then try to complete as much as you can. 
+def print_name():
+    # Your code here
+    return
+
+def multiply_numbers():
+    # Your code here
+    return
+
+def area_square(side_length):
+    # Your code here
+    return
+
+def numbers():
+    # Your code here
+    return
+
+def passing_grade(percent):
+    # Your code here
+    return
+
+def mean_list():
+    # Your code here
+    return
+
+
+
+# Put the car class here
+class Car:
+    def __init__(self):
+        return
+
+def create_car():
+    # Create a car here. Print color
+    return
+
+
+
+def fibonacci(n=100):
+    # Challenge one. Good luck
+    return
+
+
+# To run your code, just call the function here for example
+print_name()
