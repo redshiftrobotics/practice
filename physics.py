9c84b2a39c792f6e5dfa201319ea82a6a66ab961
LucaSandoval <lucasandoval@seattleacademy.org>
2020-11-12 16:14:56 -0800

    Add Robot.py & Physics.py for testing

---
+++ physics.py
@@ -0,0 +1,66 @@
+import hal.simulation
+
+from pyfrc.physics.core import PhysicsInterface
+from pyfrc.physics import motor_cfgs, tankmodel
+from pyfrc.physics.units import units
+
+
+class PhysicsEngine:
+    """
+       Simulates a 4-wheel robot using Tank Drive joystick control
+    """
+
+    def __init__(self, physics_controller):
+        """
+            :param physics_controller: `pyfrc.physics.core.Physics` object
+                                       to communicate simulation effects to
+        """
+
+        self.physics_controller = physics_controller
+
+        # Motors
+        self.lf_motor = hal.simulation.PWMSim(1)
+        # self.lr_motor = hal.simulation.PWMSim(2)
+        self.rf_motor = hal.simulation.PWMSim(3)
+        # self.rr_motor = hal.simulation.PWMSim(4)
+
+        # Gyro
+        self.gyro = hal.simulation.AnalogGyroSim(1)
+
+        # Change these parameters to fit your robot!
+        bumper_width = 3.25 * units.inch
+
+        # fmt: off
+        self.drivetrain = tankmodel.TankModel.theory(
+            motor_cfgs.MOTOR_CFG_CIM,           # motor configuration
+            110 * units.lbs,                    # robot mass
+            10.71,                              # drivetrain gear ratio
+            2,                                  # motors per side
+            22 * units.inch,                    # robot wheelbase
+            23 * units.inch + bumper_width * 2, # robot width
+            32 * units.inch + bumper_width * 2, # robot length
+            6 * units.inch,                     # wheel diameter
+        )
+        # fmt: on
+
+    def update_sim(self, now, tm_diff):
+        """
+            Called when the simulation parameters for the program need to be
+            updated.
+            
+            :param now: The current time as a float
+            :param tm_diff: The amount of time that has passed since the last
+                            time that this function was called
+        """
+
+        # Simulate the drivetrain (only front motors used because read should be in sync)
+        lf_motor = self.lf_motor.getSpeed()
+        rf_motor = self.rf_motor.getSpeed()
+
+        transform = self.drivetrain.calculate(lf_motor, rf_motor, tm_diff)
+        pose = self.physics_controller.move_robot(transform)
+
+        # Update the gyro simulation
+        # -> FRC gyros are positive clockwise, but the returned pose is positive
+        #    counter-clockwise
+        self.gyro.setAngle(-pose.rotation().degrees())
---
+++ robot.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import wpilib
+import wpilib.drive
+
+
+class MyRobot(wpilib.TimedRobot):
+    """Main robot class"""
+
+    def robotInit(self):
+        """Robot-wide initialization code should go here"""
+
+        self.lstick = wpilib.Joystick(0)
+        self.rstick = wpilib.Joystick(1)
+
+        self.lf_motor = wpilib.Jaguar(1)
+        self.lr_motor = wpilib.Jaguar(2)
+        self.rf_motor = wpilib.Jaguar(3)
+        self.rr_motor = wpilib.Jaguar(4)
+
+        l_motor = wpilib.SpeedControllerGroup(self.lf_motor, self.lr_motor)
+        r_motor = wpilib.SpeedControllerGroup(self.rf_motor, self.rr_motor)
+
+        self.drive = wpilib.drive.DifferentialDrive(l_motor, r_motor)
+
+        # Position gets automatically updated as robot moves
+        self.gyro = wpilib.AnalogGyro(1)
+
+    def autonomousInit(self):
+        """Called when autonomous mode is enabled"""
+
+        self.timer = wpilib.Timer()
+        self.timer.start()
+
+    def autonomousPeriodic(self):
+        if self.timer.get() < 0.5:
+            self.drive.arcadeDrive(0, -0.5)
+        elif self.timer.get() < 2.0:
+            self.drive.arcadeDrive(1,0)
+        else:
+            self.drive.arcadeDrive(0, 0)
+
+    def teleopPeriodic(self):
+        """Called when operation control mode is enabled"""
+        self.drive.tankDrive(-self.lstick.getY(), -self.rstick.getY())
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
